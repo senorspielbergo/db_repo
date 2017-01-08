6bf9455c9116902646d4ff3a9479f121f3a4a84d
senorspielbergo <reithmeierr@gmail.com>
2017-01-08 20:25:21 +0100

    SQl fixed

--- dev/Wahlinfo_webapp/src/main/resources/sql/insert.sql
+++ dev/Wahlinfo_webapp/src/main/resources/sql/insert.sql
@@ -4,6 +4,6 @@ AND b.partei='%partei%' AND b.titel='%titel%'
 ),
 landesliste AS (
 SELECT * FROM landesliste l JOIN wahlkreis w ON w.bundesland=l.bundesland 
-WHERE w.nummer=%wahlkreis_nr% AND l.wahljahr=2013 AND l.partei='%partei%'
+WHERE w.nummer='%wahlkreis_nr%' AND l.wahljahr=2013 AND l.partei='%partei%'
 )
-INSERT INTO Stimmzettel VALUES (DEFAULT, 2017, bewerber.id, landesliste.id, %wahlkreis_nr%);
+INSERT INTO Stimmzettel (wahljahr, direktkandidat, landesliste, wahlkreis) SELECT '2017', b.id, l.id, '%wahlkreis_nr%' FROM bewerber b, landesliste l
